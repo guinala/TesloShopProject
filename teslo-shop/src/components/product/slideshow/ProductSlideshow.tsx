f6083ac6e9cf5c9fa2df25188bb2123a202c4486
guinala <guinala@alumni.uv.es>
2026-09-11 18:35:58 +0200

    Progreso en maquetacion
    Signed-off-by: guinala <guinala@alumni.uv.es>

---
+++ teslo-shop/src/components/product/slideshow/ProductSlideshow.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+interface Props {
+    images: string[];
+    title: string;
+    className?: string;
+}
+
+export const ProductSlideshow = ({ images, title, className }: Props) => {
+  return (
+    <div className={className}>
+        <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log("swiper")}
+        >
+            <SwiperSlide />
+
+        </Swiper>
+    </div>
+  )
+}
